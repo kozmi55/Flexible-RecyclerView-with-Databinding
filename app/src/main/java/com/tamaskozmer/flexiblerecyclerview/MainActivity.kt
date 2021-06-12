827c4b1f511dcce7eea62ec62d8044bf95608ad2
Tamas Kozmer <kozmi55@gmail.com>
2021-06-12 21:21:39 +0200

    Init commit.

---
+++ app/src/main/java/com/tamaskozmer/flexiblerecyclerview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tamaskozmer.flexiblerecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
